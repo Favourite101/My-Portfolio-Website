4bf5069724b961363bbee9135186e0962dd68bf4
Oluwatunmibi Favour <ofavourite001@gmail.com>
2024-01-28 08:04:48 +0000

    Refactor code for improved performance

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Oluwatunmibi Favour Oyindamola",
+  title: "AI Engineer and Backend Developer",
+  email: "ofavourite001@gmail.com",
+  gitHub: "Favourite101",
+  instagram: "fav.fantasy",
+  linkedIn: "favourtunmibi",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "Fav_fantasy_",
+  youTube: "Oluwatunmibi Favour",
 };
 
 const primaryColor = "#4E567E";
